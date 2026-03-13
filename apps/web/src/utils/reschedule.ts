68902e8495f8c697e5228bab328f979354405b62
Joes131205 <stevenjoe1312@gmail.com>
2026-03-13 13:30:34 +0700

    feat: Initialize global and nested weighted time calculation functions

--- apps/web/src/utils/reschedule.ts
+++ apps/web/src/utils/reschedule.ts
@@ -1,1 +1,8 @@
-export const reschedule = (data) => {};
+export const calcGlobalWeightedTime = (data: any) => {
+    // Weight task x weight genre x total time / total ( all weight task x weight genre)
+    const weightArea = data.reduce((a, b) => a + b.weight, 0);
+};
+export const calcNestedWeightedTime = (data: any) => {
+    // Wight task x weight genre x total time /  (total weight task di genre tersebut x total weight genre)
+    const weightArea = data.reduce((a, b) => a + b.weight, 0);
+};
