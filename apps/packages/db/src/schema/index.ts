5c88c2b3ccd17bad5aca2f657e3e91e789507055
Joes131205 <stevenjoe1312@gmail.com>
2026-04-26 22:42:03 +0700

    fix: vercel fix 22

--- apps/packages/db/src/schema/index.ts
+++ apps/packages/db/src/schema/index.ts
@@ -1,4 +1,4 @@
 export * from "./users";
-export * from "./areas.ts";
-export * from "./subareas.ts";
-export * from "./planned_sessions.ts";
+export * from "./areas";
+export * from "./subareas";
+export * from "./planned_sessions";
